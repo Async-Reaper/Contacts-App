d53149356d17b64737dc3ea01d7f8525e8d475ff
ASRync <m_igor_r@mail.ru>
2022-04-15 00:19:47 +0700

    create loader component

---
+++ src/components/UI/Loader/Loader.tsx
@@ -0,0 +1,12 @@
+import { CircularProgress } from '@mui/material'
+import React, { FC } from 'react'
+
+const Loader: FC = () => {
+    return (
+        <div>
+            <CircularProgress />
+        </div>
+    )
+}
+
+export default Loader
